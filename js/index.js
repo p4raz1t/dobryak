b46c7d330e5d15d24dfa46459148dedc19cfade3
Zak69 <142248106+p4raz1t@users.noreply.github.com>
2024-11-15 10:16:30 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+
+$('.head').click(function(){
+  $(this).toggleClass('active');
+  $(this).parent().find('.arrow').toggleClass('arrow-animate');
+  $(this).parent().find('.content').slideToggle(280);
+});
